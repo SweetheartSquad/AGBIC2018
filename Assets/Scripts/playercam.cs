a7248ecd2e1db4848827e91e4c92c893e04ce462
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-08-12 18:45:05 -0400

    really basic controls

---
+++ Assets/Scripts/playercam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercam : MonoBehaviour {
+	private Camera cam;
+	public Transform target;
+	public Transform anchor;
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cam.transform.LookAt(target);
+		cam.transform.position = Vector3.Lerp(cam.transform.position, anchor.position, .1f);
+	}
+}
