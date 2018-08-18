c65c232b0697b6416d3159162813002cad69dcb1
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-08-18 19:30:15 -0400

    rough selfie cam

--- Assets/Scripts/playercam.cs
+++ Assets/Scripts/playercam.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class playercam : MonoBehaviour {
 	private Camera cam;
-	public Transform target;
-	public Transform anchor;
-	// Use this for initialization
+	public Transform targetShoulder;
+	public Transform anchorShoulder;
+	public float fovShoulder = 70.0f;
+	public Transform targetSelfie;
+	public Transform anchorSelfie;
+	public float fovSelfie = 55.0f;
+	public bool selfieMode = false;
 	void Start () {
 		cam = GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Transform target = selfieMode ? targetSelfie : targetShoulder;
+		Transform anchor = selfieMode ? anchorSelfie : anchorShoulder;
+		float fov = selfieMode ? fovSelfie : fovShoulder;
 		cam.transform.LookAt(target);
 		cam.transform.position = Vector3.Lerp(cam.transform.position, anchor.position, .1f);
+		cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, fov, .1f);
 	}
 }
